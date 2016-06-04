7da5709a8ce1ec856b70030f8c0ba4f5622d3135
Sebastián Osorio <im@svzosorio.co>
2016-06-04 11:32:43 -0500

    Add post to hook

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Pepe boot its working');
+});
+
+app.post('/', function(req, res){
+    try {
+        res.status(200).json({
+            speech: req.body.fulfillment + 'desde el hook',
+            displayText: req.body.fulfillment.speech + 'desde el hook',
+            source: 'pepehook'
+        });
+    } catch (e) {
+
+    }
 });
 
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+    console.log(`Example app listening on port ${port}!`);
 });
